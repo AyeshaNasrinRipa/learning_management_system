11f70d4b711509923d4496d4543bca7fe3119aaa
AyeshaNasrinRipa <1arshiayesha@gmail.com>
2023-11-08 23:52:11 +0600

    token authentication added

--- lms_api/settings.py
+++ lms_api/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,7 +120,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+REST_FRAMEWORK={
+   'DEFAULT_AUTHENTICATION_CLASSES':[
+       'rest_framework.authentication.TokenAuthentication',  
+   ],   
+}
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
