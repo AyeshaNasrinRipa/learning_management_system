1576a7458cbffbfbb24ae89d87dc5597479f9e98
AyeshaNasrinRipa <1arshiayesha@gmail.com>
2023-11-11 12:42:50 +0600

    react added

--- lms_api/main/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from . import models  
-
-class TeacherSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=models.Teacher
-        fields=['id','full_name','email','password','qualification','address']
-        
-    
